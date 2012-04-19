f8f2be07a6608e6cee447ddb6b1728dcfd96d058
johannes <jo.lahann@googlemail.com>
2012-04-19 16:55:16 +0200

    dad

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -4,7 +4,7 @@ pid "#{root}/tmp/pids/unicorn.pid"
 stderr_path "#{root}/log/unicorn.log"
 stdout_path "#{root}/log/unicorn.log"
 
-listen "/tmp/unicorn.blog.sock"
+listen "/tmp/unicorn.bachelorjohannes.sock"
 worker_processes 2
 timeout 30
 
