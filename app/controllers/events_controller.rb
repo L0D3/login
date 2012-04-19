e792e3b3b38ac260e14679bc15ecb95a5cef1fea
johannes <jo.lahann@googlemail.com>
2012-04-19 16:15:58 +0200

    dad

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,7 +1,12 @@
 class EventsController < ApplicationController
   before_filter :require_login
   def index
-    @events= User.find_by_username("USER..dfki-uk").events
+    if params[:year].blank?
+      @year=Time.now.to_a[5]
+    else
+      @year=params[:year]
+    end
+    @events= User.find_by_username("USER..dfki-uk").events.where(:date_year=>@year)
   end
 
 end
--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,6 +1,6 @@
 class PhotosController < ApplicationController
   before_filter :require_login
   def index
-    @photos=Event.find(params[:eventid]).photos
+    @photos=Event.where(:s=>params[:eventid]).select([:title,:s,:o,:collectionid,:creator,:dfki_preview]).first.photos
   end
 end
